56867429d15b1deb92d92c8064f1d8d0589d9d49
Lady Stock <nikki.castlea@gmail.com>
2020-07-05 20:50:36 -0600

    psuedo coding added

---
+++ assets/script.js
@@ -0,0 +1,17 @@
+//Assignment code
+var currentCity;// current city
+var lastSearched; //
+var fiveDayForecast;
+
+//set display for default/last searched city forecast
+
+//search bar for looking up cities
+
+//set display for current city weather. Include city name, date, icon of weather, temperature, humidity, wind speed, UV index
+
+//color coded UV index: favorable, moderate, severe
+
+
+//5 day forecast for selected city. Include: dates, temperature, icon of weather conditions, humidity
+
+//show search history
