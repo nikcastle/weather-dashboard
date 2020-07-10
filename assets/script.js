08b9575e515a09a93cc8d59020be2b9dc4c42447
Lady Stock <nikki.castlea@gmail.com>
2020-07-09 21:42:11 -0600

    color coding for UV index fixed, added description beneath icon

--- assets/script.js
+++ assets/script.js
@@ -2,82 +2,93 @@
 var currentCity;
 var lastSearched = JSON.parse(localStorage.getItem("city")) || [];
 var userLocation = $(this).attr("data-name");
+var p = "Five Day Future Cast";
 
-
-//set display for default/last searched city forecast
 
 
 //set display for current city weather. Include city name, date, icon of weather, temperature, humidity, wind speed, UV index
-function currentWeather () {
+function currentWeather() {
     var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + currentCity + "&units=imperial&appid=e318c6cc07b96c8c279bb20fa2877307"
     $.ajax({
         url: queryURL,
         method: "GET"
-    }).then(function(response) {
+    }).then(function (response) {
+        $("#todayCard").remove();
         console.log(response);
-        $("#clear").remove();
         var day = response;
-           console.log(day.main.temp);
-           var card = $("<div id='clear'>").addClass("col-md-9 card");
-           var cardBody = $("<div id='today'>").addClass("card-body");
-           var date = $("<h3>").addClass("card-title").text("Today's Weather");
-           var temp = $("<h4>")
-                .addClass("card-subtitle mb-2")
-                .text(Math.round(day.main.temp) + "˚F");
-            card.append(cardBody.append(date, temp));
-            $("#main").append(card);
-            var lat = day.coord.lat;
-            var long = day.coord.lon;
-    
-            //color coded UV index: favorable, moderate, severe
-            return $.ajax({
-                url:"https://api.openweathermap.org/data/2.5/uvi?appid=e318c6cc07b96c8c279bb20fa2877307&lat=" + lat +"&lon=" + long,
-                method: "GET"
-            })
-    }).then(function(UVindex) {
-        console.log(UVindex);
-        indexVal = $("<p>").addClass("card-text").text(UVindex.value);
+        var card = $("<div id='todayCard'>").addClass("col-md-9 card");
+        var cardBody = $("<div id='today'>").addClass("card-body");
+        var date = $("<h3>").addClass("card-title").text("Today's Weather for ").append(currentCity);
+        var temp = $("<h4>")
+            .addClass("card-subtitle mb-2")
+            .text(Math.round(day.main.temp) + "˚F");
+        var hum = $("<p>").addClass("card-text").text("Humidity: " + day.main.humidity + "%")
+        var windSpeed = $("<p>").addClass("wind").text("Wind Speed: " + Math.round(day.wind.speed) + " mph")
+        var icon = $("<img>")
+        icon.attr("src", "http://openweathermap.org/img/wn/" + day.weather[0].icon + "@2x.png");
+        var iconText = $("<p>")
+        iconText.text(day.weather[0].description);
+        card.append(cardBody.append(date, temp, hum, windSpeed, icon, iconText));
+        $("#main").append(card);
+        var lat = day.coord.lat;
+        var long = day.coord.lon;
+
+        //color coded UV index: favorable, moderate, severe
+        return $.ajax({
+            url: "https://api.openweathermap.org/data/2.5/uvi?appid=e318c6cc07b96c8c279bb20fa2877307&lat=" + lat + "&lon=" + long,
+            method: "GET"
+        })
+    }).then(function (UVindex) {
+        indexVal = $("<p>");
+        indexVal.text("UV Index: " + UVindex.value).addClass("card-text");
+      
         //add if statements for color coding
+        if (UVindex.value <= 3) {
+            indexVal.addClass("favorable").append(" Favorable");
+        } else if (UVindex.value > 3 && UVindex.value < 7) {
+            indexVal.addClass("moderate").append(" Moderate");
+        } else if (UVindex.value > 7) {
+            indexVal.addClass("severe").append(" Severe");
+        }
 
         $("#today").append(indexVal);
+
     })
 
 }
 
-//IN BOTH CURRENT AND FIVE DAY FUNCTIONS
-//icons for weather
-//make an img tag and set src = api link for weather icons
-//append img to card body
-
-
 
 //5 day forecast for selected city. Include: dates, temperature, icon of weather conditions, humidity
-function fiveDay () {
-    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + currentCity + "&units=imperial&appid=e318c6cc07b96c8c279bb20fa2877307"; 
-    console.log(currentCity);
-    
+function fiveDay() {
+    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + currentCity + "&units=imperial&appid=e318c6cc07b96c8c279bb20fa2877307";
+
+
     $.ajax({
         url: queryURL,
         method: "GET"
-    }).then(function(response) {
-        console.log(response);
+    }).then(function (response) {
         $("#five-day").empty();
-       for(var i = 0; i < 40; i+=8) {
-           var day = response.list[i];
-           console.log(day.main.temp);
-           var card = $("<div>").addClass("col-md-2 card");
-           var cardBody = $("<div>").addClass("card-body");
-           var date = $("<h5>").addClass("card-title").text(day.dt_txt.split(" ")[0]);
-           var temp = $("<h6>")
-                .addClass("card-subtitle mb-2 text-muted")
-                .text(Math.round(day.main.temp) + "˚F");
-            card.append(cardBody.append(date, temp));
+        
+        for (var i = 0; i < 40; i += 8) {
+            var day = response.list[i];
+            var card = $("<div>").addClass("col-md-2 card week");
+            var cardBody = $("<div>").addClass("card-body");
+            var date = $("<h5>").addClass("card-title").text(day.dt_txt.split(" ")[0]);
+            var temp = $("<h6>")
+            .addClass("card-subtitle mb-2")
+            .text(Math.round(day.main.temp) + "˚F");
+            var hum = $("<p>").addClass("card-subtitle text-muted").text(day.main.humidity + "% Humidity");
+            var icon = $("<img>")
+            icon.attr("src", "http://openweathermap.org/img/wn/" + day.weather[0].icon + "@2x.png");
+            var iconText = $("<p>")
+            iconText.text(day.weather[0].description);
+            card.append(cardBody.append(date, temp, icon, iconText, hum));
             $("#five-day").append(card);
-       }
-    
+        }
+
     });
-    
-    
+
+
 }
 //Card headers can be styled by adding .card-header to <h*> elements.
 
@@ -91,6 +102,7 @@ function searchHistory() {
         a.attr("data-name", lastSearched[i]);
         a.text(lastSearched[i]);
         $("#history-view").append(a);
+
     }
 }
 
@@ -100,16 +112,22 @@ $("#add-city").on("click", function (event) {
     currentCity = $("#user-input").val().trim();
 
 
-    if(!lastSearched.includes(currentCity)) {
+    if (!lastSearched.includes(currentCity)) {
         lastSearched.push(currentCity);
     }
+    if (lastSearched.length > 5) {
+        lastSearched.shift();
+    }
+        
+    
+   
 
     searchHistory();
-    
+
     currentWeather();
     fiveDay();
-    localStorage.setItem("city", JSON.stringify(lastSearched)); 
-    
+    localStorage.setItem("city", JSON.stringify(lastSearched));
+
     $("#user-input").val("");
 });
 
@@ -117,9 +135,8 @@ $("#add-city").on("click", function (event) {
 
 
 searchHistory();
-$(document).on("click", ".cityName", function() {
+$(document).on("click", ".cityName", function () {
     currentCity = $(this).text();
-    console.log(currentCity);
     currentWeather();
     fiveDay();
 });
